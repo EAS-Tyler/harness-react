386ba21a6438854f12e64528edfcce2606e9681b
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2025-07-03 11:31:26 +0100

    working-init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-This is harness demo 
+This is harness demo - V2
         </p>
         <a
           className="App-link"
